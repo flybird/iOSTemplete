b81bd186d52d9812cc7d6b7c3e3d7300fbff1b1a
flybird <bnseobto@gmail.com>
2013-02-07 18:40:47 +0800

    change lua script and image

--- testlua/testlua/Resources/FirstScene.lua
+++ testlua/testlua/Resources/FirstScene.lua
@@ -1,9 +1,8 @@
-
 function createFirstScene()
     local visibleSize = CCDirector:sharedDirector():getVisibleSize()
     local origin = CCDirector:sharedDirector():getVisibleOrigin()
     
-    local container = CCLayer:create()
+    local container = CCLayerColor:create(ccc4(60,40,60,255))
     
     -- CCLabel
     local mylabel = CCLabelTTF:create("First Scene", "Sans", 28)
@@ -11,18 +10,45 @@ function createFirstScene()
     container:addChild(mylabel)
     
     -- bg
+    bgTable = {}
     for i=1,5 do
         for j=1,4 do
             local cardbg = CCSprite:create("b_big_08.png")
-            cardbg:setPosition(visibleSize.width/2+40*i - 40*5/2,visibleSize.height/2+54*j-120)
-            cardbg:setScale(0.5);
+            contentSize = CCSize(32,42)
+            cardbg:setContentSize(contentSize);
+            cardbg:setPosition(visibleSize.width/2+46*i - 46*5/2,visibleSize.height/2+60*j-140)
+            --cardbg:setScale(0.5);
+            --cardbg:setContentSize(CCRectMake(0,0,40,40));
+            cardbg:setColor(ccc3(20,20,20));
             container:addChild(cardbg)
+            table.insert(bgTable,cardbg)
         end
     end
     
     -- touch events
     local function onTouchBegan(x, y)
-        cclog("onTouchBegan: %0.2f, %0.2f", x, "\t", y)
+        --print("onTouchBegan:", x, "\t", y)
+        touchLocation = CCDirector:sharedDirector():convertToGL(CCPoint(x,y))
+        for i=1,table.getn(bgTable) do
+            boundingBox = bgTable[i]:boundingBox()
+            --print("boundingBox:",boundingBox.origin.x);
+            
+            --contentSize = bgTable[i]:getContentSize()
+            
+            --contentPoint = CCPoint(8,13)
+            --contentPoint = CCPoint(boundingBox.origin.x,boundingBox.origin.y)
+            --bgTable[i]:setContentSize(contentSize);
+            --bgTable[i]:setPosition(contentPoint);
+            
+            --print("contentSize:",contentSize.height);
+            
+            if bgTable[i]:boundingBox():containsPoint(CCPoint(x,y)) then
+                --print("boundingBox:",boundingBox.origin);
+                --print("boundingBox:",boundingBox.size);
+                print("clicked bg:",i)
+                break;
+            end
+        end
     end
     
     local function onTouchMoved(x, y)
@@ -30,7 +56,7 @@ function createFirstScene()
     end
     
     local function onTouchEnded(x, y)
-        cclog("onTouchEnded: %0.2f, %0.2f", x, "\t", y)
+        --print("onTouchEnded:", x, "\t", y)
     end
     
     local function onTouch(eventType, x, y)
@@ -42,7 +68,7 @@ function createFirstScene()
             return onTouchEnded(x, y)
         end
     end
-    container:registerScriptTouchHandler(onTouch)
+    container:registerScriptTouchHandler(onTouch,false)
     container:setTouchEnabled(true)
     
     -- Back button
@@ -56,9 +82,12 @@ function createFirstScene()
     menuPopup:setPosition(30, visibleSize.height - 30)
     container:addChild(menuPopup)
     
-    -- change scene
-    local firstScene = CCScene:create()
-    firstScene:addChild(container)
+    local scene = CCScene:create()
+    scene:addChild(container)
     
-    return firstScene
+    return scene
 end
+
+
+
+
--- testlua/testlua/Resources/SecondScene.lua
+++ testlua/testlua/Resources/SecondScene.lua
@@ -17,9 +17,43 @@ function createSecondScene()
     local menuPopup = CCMenu:createWithItem(menuPopupItem)
     menuPopup:setPosition(30, visibleSize.height - 30)
     
-    local secondScene = CCScene:create()
-    secondScene:addChild(mylabel)
-    secondScene:addChild(menuPopup)
+    local scene = CCScene:create()
+    scene:addChild(mylabel)
+    scene:addChild(menuPopup)
     
-    return secondScene
+    return scene
 end
+
+
+--[[
+--
+-- the following code is about onEnter/onExit,but it's not work, The eventType always is nil;
+-- reference: http://www.cocos2d-x.org/projects/cocos2d-x/wiki/New_Lua_Engine_Documents
+--
+local function createScene()
+    local scene = CCScene:create()
+    local function sceneEventHandler(eventType)
+        print("kCCNodeOnEnter:",kCCNodeOnEnter)
+        print("evnetType:",evnetType)
+        if eventType == kCCNodeOnEnter then
+            if scene.onEnter then scene:onEnter() end
+            elseif evetType == kCCNodeOnExit then
+            if scene.onExit then scene:onExit() end
+        end
+        
+    end
+    scene:registerScriptHandler(sceneEventHandler)
+    return scene
+end
+
+local scene = createScene()
+function scene:onEnter()
+    print("on scene enter")
+end
+function scene:onExit()
+    print("on scene exit")
+end
+
+CCDirector:sharedDirector():runWithScene(scene)
+
+--]]
